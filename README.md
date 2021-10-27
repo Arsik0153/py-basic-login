a5ded2247f854106057b4c2e52048985b4f828d8
Zhanarys Sairanbekov <sairanbekov@taptima.ru>
2021-10-27 23:48:31 +0600

    Code refactoring

--- README.md
+++ README.md
@@ -1,30 +1,18 @@
 # CoinGecko
 
 ## Authors
-    Authors:
     Tursynbay Yernar SE-2003
     Kadyrbergenova Nurayim SE-2001
     Sairanbekov Zhanarys SE-2003
 
 ## Installation
 ```sh
-pip install pycoingecko
+pip install sqlite3
 ```
 
 ## Usage
 ```sh
-cd api/src/
 python main.py
-```
-
-## Examples
-```sh
-Enter number to filter: 5 
-btc --- current price:  47920 $
-eth --- current price:  3604.25 $
-ada --- current price:  2.47 $
-usdt --- current price:  1.0 $
-bnb --- current price:  425.92 $
 ```
 
 ## License
--- web_server.py
+++ web_server.py
@@ -9,11 +9,11 @@ import psycopg2
 import jwt
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:zf9392zf@localhost/pythonDB'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'POSTGRESQL_URL'  # secret url
 db = SQLAlchemy(app)
 
 conn = psycopg2.connect(host="localhost", port=5432,
-                        database="pythonDB", user="postgres", password="zf9392zf")
+                        database="db", user="postgres", password="password")
 cur = conn.cursor()
 cur.execute("SELECT * FROM Users")
 query_results = cur.fetchall()
@@ -21,9 +21,7 @@ query_results = cur.fetchall()
 
 @app.route('/login')
 def login():
-
     auth = request.authorization
-
     for i in range(cur.rowcount):
         if auth and auth.username == query_results[i][1]:
             if auth.password == query_results[i][2]:
@@ -32,16 +30,10 @@ def login():
 
                 sql = "UPDATE Users SET token = %s WHERE id = %s"
                 val = (token, query_results[i][0])
-                print(val)
                 cur.execute(sql, val)
                 conn.commit()
 
                 return jsonify({'token': token})
-
-    # if auth and auth.password == 'password':
-    # token = jwt.encode({'user':auth.username, 'exp':datetime.utcnow() + timedelta(minutes=30)}, app.config['SECRET_KEY'])
-
-    # return jsonify({'token': token})
 
     return make_response('Could not verify!', 401, {'WWW-Authenticate': 'Basic realm="Login required'})
 
@@ -50,16 +42,11 @@ def login():
 def protected():
     cur.execute("SELECT * FROM Users")
     query_results = cur.fetchall()
-
     token = request.args.get('token')
-
     for j in range(3):
-        print(token)
-        print(query_results[j][3])
         if token == query_results[j][3]:
             return "<h1>Hello, token which is provided is correct </h1>"
         else:
-            print("NE VIZU")
             continue
     return "<h1>Hello, Could not verify the token </h1>"
 
